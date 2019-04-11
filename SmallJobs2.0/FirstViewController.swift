35adadfa6d437f288d17cbe14c88050a48eb24cd
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-04-11 11:07:05 -0400

    Initial Commit

---
+++ SmallJobs2.0/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SmallJobs2.0
+//
+//  Created by Austin Rightnowar on 4/11/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SmallJobs2.0/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SmallJobs2.0
+//
+//  Created by Austin Rightnowar on 4/11/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
