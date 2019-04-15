22417f46fc6d7f4154cc97f4c7401c6f7ad03986
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-04-15 10:55:15 -0400

    working on messaging

---
+++ SmallJobs2.0/Controllers/NewMessageToBoss.swift
@@ -0,0 +1,33 @@
+//
+//  NewMessageToBoss.swift
+//  SmallJobs2.0
+//
+//  Created by Austin Rightnowar on 4/15/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class NewMessageToBoss: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
